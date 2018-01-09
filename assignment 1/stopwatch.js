7fc65fecae758845cc7e48697d237717950350ed
Elton Bezerra <elton.cin@hotmail.com>
2018-01-09 12:54:29 -0200

    first commit

---
+++ assignment 1/stopwatch.js
@@ -0,0 +1,58 @@
+//global variables
+var started = false;
+var time = 0;
+var interval;
+var recordedTimes;
+
+function setUp() {
+    var buttons = document.getElementsByTagName("button");
+
+    buttons[0].addEventListener('click', () =>  startStop());// BUTTON START/STOP
+    buttons[1].addEventListener('click', () => reset());// BUTTON RESET
+    buttons[2].addEventListener('click', () => recordCurrentTime());// BUTTON RECORD TIME
+    
+
+    document.addEventListener('keydown', function (event) {
+        if (event.key.toLowerCase() == 's') startStop();
+        if (event.key.toLowerCase() == 'r') reset();
+        if (event.key.toLowerCase() == 't') recordCurrentTime();
+    });
+
+
+
+}
+
+function startStop() {
+    if (started == false) {
+        started = true;
+        interval = setInterval(function () {
+            time += 0.01;
+            document.getElementById("tempo").innerHTML = time.toFixed(2);
+        }, 10);
+    } else {
+        clearInterval(interval);
+        started = false;
+    }
+}
+
+
+function reset() {
+    time = 0;
+    started = false;
+    document.getElementById("tempo").innerHTML = time.toFixed(2);
+    recordedTimes = document.getElementById("recordedTimes").innerHTML = '';
+    clearInterval(interval);
+}
+
+function recordCurrentTime() {
+    if (started) {
+        recordedTimes = document.getElementById("recordedTimes");
+        var li = document.createElement("li");
+        li.textContent = time.toFixed(2);
+        recordedTimes.appendChild(li);
+    } else {
+        return;
+    }
+}
+
+setUp();
